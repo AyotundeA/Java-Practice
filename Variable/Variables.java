84b9095366e3d1cd2c64760a0e8ea9a5f135e9bb
Ayotunde <121546620+AyotundeA@users.noreply.github.com>
2023-02-07 07:43:54 +0000

    Create Variables.java

---
+++ Variable/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+	public static void main(String[] args) {
+		//This is the script that contains variables.
+		String string = new String("This is a string");
+		String Amazing = "Wow, that was amazing!" + string;
+		System.out.println(Amazing);
+	}
+}
