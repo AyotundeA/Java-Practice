d2b7423fd7c5e814d35901ca12befc78a144cd0f
Ayotunde <121546620+AyotundeA@users.noreply.github.com>
2023-02-07 07:36:44 +0000

    Create WhileLoop.java

---
+++ Loops/WhileLoop.java
@@ -0,0 +1,12 @@
+
+public class WhileLoop {
+	
+	public static void main(String[] args) {
+		//While Example
+		int i = 1;
+	while(i < 11) {
+		System.out.println("iteration " + i);
+		i++;
+		}
+	}
+}
