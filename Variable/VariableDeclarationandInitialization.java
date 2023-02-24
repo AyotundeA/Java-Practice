f7f54dd7653e8356a876ff19c2e220b6ebfc41f4
Ayotunde <121546620+AyotundeA@users.noreply.github.com>
2023-02-24 07:28:18 +0000

    Create VariableDeclarationandInitialization.java

---
+++ Variable/VariableDeclarationandInitialization.java
@@ -0,0 +1,11 @@
+
+public class VariableDeclarationandInitialization {
+	public static final double PI = 3.14159;
+	private int x = 10;
+	public static void main(String[] args) { 
+		
+		int x;//Method
+		x = 5; //initialization
+		x = 10;
+	}
+}
